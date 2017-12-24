6a444353c680ec6eeeff891efa95e2ad6b594d8e
Nicholas Bishop <nicholasbishop@gmail.com>
2017-12-24 01:50:41 -0500

    Add -l/--list option

--- sate/app.py
+++ sate/app.py
@@ -12,11 +12,22 @@ def load_satefile(path):
         return parse.load_satefile(rfile)
 
 
+def print_targets(satefile):
+    for target in satefile.targets:
+        print(target)
+
+
 def run():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--list', help='list targets',
+                        action='store_true')
     parser.add_argument('target', nargs='?')
     parser.add_argument('args', nargs=argparse.REMAINDER)
     args = parser.parse_args()
 
     satefile = load_satefile(find_satefile())
-    satefile.run(args.target, args.args)
+
+    if args.list:
+        print_targets(satefile)
+    else:
+        satefile.run(args.target, args.args)
